829ec929873c64fe0d98547cc992cc6e9737e9fd
Reavis Sutphin-Gray <reavis@sutphin-gray.com>
2013-12-17 21:07:05 -0800

    Avoid using deprecated django.conf.urls.defaults
    Import from django.conf.urls on Django >= 1.4 and fall back to django.conf.urls.defaults for older versions

--- paypal/standard/ipn/tests/test_urls.py
+++ paypal/standard/ipn/tests/test_urls.py
@@ -1,4 +1,7 @@
-from django.conf.urls.defaults import *
+try:
+    from django.conf.urls import *
+except ImportError:  # Django < 1.4
+    from django.conf.urls.defaults import *
 
 urlpatterns = patterns('paypal.standard.ipn.views',
     (r'^ipn/$', 'ipn'),
